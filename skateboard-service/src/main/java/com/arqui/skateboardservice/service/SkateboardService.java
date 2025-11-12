7eb2d4ecb433441af22b029eb3df0ffaf3f158e9
Juan Manuel Santa Cruz <juanmasantax@gmail.com>
2025-11-12 18:32:03 -0300

    Modularizado metodo static distanceKm dentro de utils

--- skateboard-service/src/main/java/com/arqui/skateboardservice/service/SkateboardService.java
+++ skateboard-service/src/main/java/com/arqui/skateboardservice/service/SkateboardService.java
@@ -15,6 +15,8 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import static com.arqui.skateboardservice.utils.calculateDistanceKm.distanceKm;
+
 @Service
 public class SkateboardService {
 
@@ -112,16 +114,6 @@ public class SkateboardService {
                 .collect(Collectors.toList());
     }
 
-    private double distanceKm(double lat1, double lon1, double lat2, double lon2) {
-        double R = 6371; // El radio de la tierra
-        double dLat = Math.toRadians(lat2 - lat1);
-        double dLon = Math.toRadians(lon2 - lon1);
-        double a = Math.sin(dLat / 2) * Math.sin(dLat / 2)
-                + Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2))
-                * Math.sin(dLon / 2) * Math.sin(dLon / 2);
-        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-        return R * c;
-    }
 
 
 }
